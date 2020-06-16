b25b0ac36f5590962e21af3aa097779239e585e1
Marta <36074465+zabkaxp@users.noreply.github.com>
2020-06-16 23:49:43 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<?php wp_head(); ?>
+</head>
+<body>
+  <header class="site-header">
+    <div class="banner-area">
+    <div class="page-banner" style="background-image: url(<?php echo get_theme_file_uri('/images/logo.png') ?>);"></div>
+    </div>
+    <div class="wrapper">
+      <input type="checkbox" id="nav-toggle" class="nav-toggle">
+      <div class="nav-top">
+      <nav>
+        <ul>
+         <li><a href="#">1Polska</a></li>
+         <li><a href="#">Pafere</a></li>
+         <li><a href="#">Podziemna TV</a></li>
+         <li><a href="#">NISS</a></li>
+         <li><a href="#">Nawigatorzy jutra</a></li>
+        </ul>
+      </nav>
+      </div>
+      <div class="nav-bottom">
+      <nav>
+        <ul>
+         <li><a href="#">Polska</a></li>
+         <li><a href="#">Świat</a></li>
+         <li><a href="#">Religie</a></li>
+         <li><a href="#">Zdrowie</a></li>
+         <li><a href="#">Gospodarka</a></li>
+         <li><a href="#">Historia</a></li>
+         <li><a href="#">Technologia</a></li>
+         <li><a href="#">Szukaj</a></li>
+        </ul>
+      </nav>
+      </div>
+  <label for="nav-toggle" class="nav-toggle-label">
+  <span></span>
+
+
+  </label>
+    </div>
+    </header>
+
